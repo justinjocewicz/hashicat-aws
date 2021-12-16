3287b3015a8bb3924e4f9715ae9af30271b0e102
Justin Jocewicz <justin.jocewicz@gmail.com>
2021-12-16 22:15:42 +0000

    s3 bucket prefix

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,4 +2,6 @@ module "s3-bucket" {
   source  = "app.terraform.io/hashicat-demo-justinj/s3-bucket/aws"
   version = "2.11.1"
   # insert required variables here
+  bucket_prefix = justinjocewicz
+
 }
