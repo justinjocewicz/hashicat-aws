53582b9b0fd6c8826764c1c48f682ef12e2a07fd
Justin Jocewicz <justin.jocewicz@gmail.com>
2021-12-16 21:30:33 +0000

    capitalized tags

--- main.tf
+++ main.tf
@@ -129,8 +129,8 @@ resource "aws_instance" "hashicat" {
 
   tags = {
     Name = "${var.prefix}-hashicat-instance"
-    department  = "devops"
-    billiable   = "justinj"
+    Department  = "devops"
+    Billiable   = "justinj"
   }
 }
 
